b6ee78a0967a8eb6fab7037258774ef5005c0ff8
tevaitea_doom <tevaitea.doom@coda-student.school>
2026-01-12 16:37:53 +0100

    skin

---
+++ TP/TP5/Player.js
@@ -0,0 +1,14 @@
+class player {
+    constructor(id ,skinID, atk , cooldown , speed , hp , hps , position){
+        this.id = id;
+        this.pseudo = tevaito;
+        this.skinID = skinID;
+        this.atk = atk;
+        this.cooldown = cooldown;
+        this.speed = speed;
+        this.hp = hp;
+        this.hps = hps;
+        this.position = ;
+    }
+}
+
