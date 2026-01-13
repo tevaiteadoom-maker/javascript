83bb855e6474f02b88ae955268033aa5adc855d4
tevaitea_doom <tevaitea.doom@coda-student.school>
2026-01-13 13:37:50 +0100

    TP5

---
+++ TP/TP5/Game.js
@@ -0,0 +1,3 @@
+let woleSpriteDuration; // durée entre deux frames (ms)
+let woleSpriteIndex;    // frame courante
+let woleSpriteNumber;   // nombre total de frames
--- TP/TP5/Player.js
+++ TP/TP5/Player.js
@@ -1,14 +1,63 @@
-class player {
-    constructor(id ,skinID, atk , cooldown , speed , hp , hps , position){
+class Player {
+    constructor(id, pseudo, skinID, atk, cooldown, speed, hp, hps, position) {
         this.id = id;
-        this.pseudo = tevaito;
+        this.pseudo = pseudo;
         this.skinID = skinID;
+        // position attendue : { x: number, y: number }
+        this.position = position;
         this.atk = atk;
         this.cooldown = cooldown;
         this.speed = speed;
         this.hp = hp;
         this.hps = hps;
-        this.position = ;
-    }
-}
 
+    }
+
+    // Met à jour le joueur avec les données envoyées par le serveur
+    update(data) {
+        this.atk = data.atk;
+        this.cooldown = data.cooldown;
+        this.speed = data.speed;
+        this.hp = data.hp;
+        this.hps = data.hps;
+        this.position = data.position;
+    }
+
+    animate() {
+        //the player is walking
+        if(this.iswalking){
+
+            this.currentWalkSpriteStep++;
+            if(this.currentWalkSpriteStep >= this.walkSpriteDuration){
+                this.currentWalkSpriteStep = 0;
+                this.walkSpriteIndex++;
+            }
+            this.walkSpriteIndex ++;
+            if(this.walkSpriteIndex >= this.walkSpriteNumber){
+                this.walkSpriteIndex = 0;
+            }
+        }
+        //the player is attaking
+        else if(this.isAttaking){
+
+        }
+        //the player is dying
+        else if(this.Dying){
+
+        }
+        // idle
+        else {
+
+        }
+        console.log("Walk Animation: \n");
+        console.log("isWalking =  ", this.isWalking);
+        console.log("walkSpriteIndex = ", this.walkSpriteIndex, "/", this.walkSpriteNumber);
+        console.log("this.currentWalkSpiteStep = ", history.currentWalkSpriteStep, "/", this.walkSpriteDuration);
+    }
+
+}
+tevaito = new Player(41, "tevaito", 1, [0,0]);
+
+for (let i = 0; i < 10; i++){
+    tevaito.animate();
+}
