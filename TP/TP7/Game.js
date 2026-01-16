459dd11cec6b3c037b6f52cef062b6647c5000b2
tevaitea_doom <tevaitea.doom@coda-student.school>
2026-01-16 14:54:32 +0100

    test

---
+++ TP/TP7/Game.js
@@ -0,0 +1,46 @@
+// Exemple de message recu par le backend, à utiliser pour vos tests :
+const backendData = {
+   "isRunning":true,
+   "isOver":false,
+   "timer":190.6000000000091,
+   "players":{
+      "3cd71bbb-6a6b-4d4e-80e3-107130328a27":{
+         "name":"blabla",
+         "skinPath":"./spritesheets/3.png",
+         "position":[
+            0.5600000000000003,
+            0.17999999999999977
+         ],
+         "lvl":1,
+         "hp":100,
+         "maxHp":100,
+         "hpRegenRate":10,
+         "speed":0.2,
+         "direction":3,
+         "isAttacking":false,
+         "isWalking":false,
+         "isDying":false,
+         "attackCooldown":1,
+         "currentAttackCooldown":0
+      },
+      "28ead291-fcea-4b41-a596-d3c876c49a53":{
+         "name":"bloublou",
+         "skinPath":"./spritesheets/4.png",
+         "position":[
+            0.44,
+            0.19
+         ],
+         "lvl":1,
+         "hp":100,
+         "maxHp":100,
+         "hpRegenRate":10,
+         "speed":0.2,
+         "direction":0,
+         "isAttacking":false,
+         "isWalking":false,
+         "isDying":false,
+         "attackCooldown":1,
+         "currentAttackCooldown":0
+      }
+   }
+};
